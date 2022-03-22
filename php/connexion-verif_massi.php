286db6b200ae6b1462a6471b88a39f5a9a8de1ed
Dino53yx <dino53yx@gmail.com>
2022-03-22 09:51:53 +0100

    Update connexion-verif_massi.php

--- php/connexion-verif_massi.php
+++ php/connexion-verif_massi.php
@@ -4,18 +4,18 @@ include 'config.php';
 $json = file_get_contents('php://input');
 $obj = json_decode($json,true);
 // Populate User nom from JSON $obj array and store into $nom.
-$nom = $obj['nom'];
-
- 
+$nom = $obj['nom']; 
 // Populate Password from JSON $obj array and store into $password.
 $password = $obj['password'];
-$result = PDO::query("SELECT * FROM utilisateurs WHERE nom = '$nom' and password = '$password'");
-if ($result->num_rows==0){
-   
-    echo json_encode('Mauvaises Informations');
-    else{
-    echo json_encode('ok');
-    }
+
+if ($obj['nom']!=""){
+    $result = PDO::query("SELECT * FROM utilisateurs WHERE nom = '$nom' and password = '$password'");
+        if ($result->num_rows==0){
+            echo json_encode('Mauvaises Informations');
+        }
+        else{
+            echo json_encode('ok');
+        }
 }
 else{
     echo json_encode('ressayer');
