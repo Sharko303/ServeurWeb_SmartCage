daaf2ab67ff16a1832ce455fbd25a22d4946bcac
PINNA <sn_2_g2_06@snir.local>
2022-04-05 15:21:55 +0200

    Update connexion_api.php

--- php/mobile_api/connexion_api.php
+++ php/mobile_api/connexion_api.php
@@ -1,6 +1,8 @@
 <?php
 // Importing DBConfig.php file.
 include '../config.php';
+class CConnexion
+{
 $json = file_get_contents('php://input');
 $obj = json_decode($json,true);
 // Populate User nom from JSON $obj array and store into $nom.
@@ -8,24 +10,25 @@ $nom = $obj['nom'];
 // Populate Password from JSON $obj array and store into $password.
 $password = hash('sha256',$obj['password']);
 
-if ($obj['nom']!=""){
-    $result = $bdd->query("SELECT * FROM utilisateurs WHERE nom = '$nom' and password = '$password'");
+    if ($obj['nom']!=""){
+            $result = $bdd->query("SELECT * FROM utilisateurs WHERE nom = '$nom' and password = '$password'");
         
-    if ($result->rowCount()==0){
-            echo json_encode('Mauvaises Informations');
-        }
-        else{
-            $userinfo = $result->fetch();
-            if($userinfo['type'] == 'entraineur'){
-            echo json_encode('entraineur');
+        if ($result->rowCount()==0){
+                echo json_encode('Mauvaises Informations');
             }
-            elseif($userinfo['type'] == 'joueur'){
-            echo json_encode('joueur');
+            else{
+                $userinfo = $result->fetch();
+                if($userinfo['type'] == 'entraineur'){
+                    echo json_encode('entraineur');
+                }
+                elseif($userinfo['type'] == 'joueur'){
+                    echo json_encode('joueur');
+                }
             }
         }
-}
 
-else{
-    echo json_encode('reessayer');
+    else{
+        echo json_encode('reessayer');
+    }
 }
 ?>
