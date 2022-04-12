5384a51116ae31dcddf849adaade5caadae7ad5c
Sharko303 <thibaultfontaine303@gmail.com>
2022-04-12 13:44:21 +0200

    test_graphique

--- php/entraineur/CProgression.php
+++ php/entraineur/CProgression.php
@@ -35,55 +35,7 @@ class CProgression
     }
     function afficherGraph($joueur)
     {
-        $graph = new Graph(900,500);
-
-// Réprésentation linéaire
-
-$graph->SetScale("textlin");
-
-// Fixer les marges
-
-$graph->img->SetMargin(40,30,50,120);
-
-// Création du graphique histogramme
-
-$bplot = new BarPlot($tableauNb);
-
-// Ajouter les barres au conteneur
-
-$graph->Add($bplot);
-
-// Spécification des couleurs des barres
-
-$bplot->SetFillColor(array('#5EB6DD'));
-
-// Afficher les valeurs pour chaque barre
-
-$bplot->value->Show();
-
-// Modifier le rendu de chaque valeur
-
-$bplot->value->SetFormat('%d');
-
-// Le titre
-
-$graph->title->Set("Inventaire Télédistribution");
-
-$graph->title->SetFont(FF_ARIAL,FS_BOLD);
-
-// Taille réduite pour l'axe horizontal(axe x) et vertical (axe y) et mise à 45° des labels
-
-$graph->xaxis->SetTickLabels($tableauElement);
-
-$graph->xaxis->SetLabelAngle(45);
-
-$graph->xaxis->SetFont(FF_ARIAL,FS_BOLD,6);
-
-$graph->yaxis->SetFont(FF_ARIAL,FS_BOLD,6);
-
-// Afficher le graphique
-
-$graph->Stroke();     
+        
     }
 }
 ?>
