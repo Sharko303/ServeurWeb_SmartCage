bfc40178efba1f7aa1ffda7ca0a13d4c1b43116b
Sharko303 <thibaultfontaine303@gmail.com>
2022-06-03 12:46:23 +0200

    Update CJoueur.php

--- php/entraineur/CJoueur.php
+++ php/entraineur/CJoueur.php
@@ -6,8 +6,7 @@
       
    $dsn = "mysql:host=$host;dbname=$dbname"; 
    // récupérer tous les utilisateurs
-$sql = 'SELECT nom, prenom, categorie FROM utilisateurs WHERE type = "joueur"';
-    
+   $sql = 'SELECT nom, prenom, categorie FROM utilisateurs WHERE type = "joueur"';
    try{
     $bdd = new PDO($dsn, $username, $password);
     $stmt = $bdd->query($sql);
